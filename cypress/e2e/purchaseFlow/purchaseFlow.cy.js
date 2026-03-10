36f07e9fd6548b03eba963e3aa9abfef061a5cf0
brunockutzke <brunokutzke@gmail.com>
2026-03-10 11:40:09 -0300

    Adicionando comando customizado para preencher os dados do cartão e caso de teste para compra com pagamento através de cartão de crédito.

--- cypress/e2e/purchaseFlow/purchaseFlow.cy.js
+++ cypress/e2e/purchaseFlow/purchaseFlow.cy.js
@@ -4,7 +4,7 @@ import locators from "../../support/locators/locators"
 
 describe("Purchase Flow", () => {
 
-  it("Fluxo de compra", () => {
+  it("Fluxo de compra - Pagamento com saldo global", () => {
     cy.env(['username', 'password']).then(({ username, password}) => {
         
         cy.login(username, password)
@@ -84,6 +84,92 @@ describe("Purchase Flow", () => {
             .siblings()
             .first()
             .should("have.text", "Pagamento Realizado com sucesso")
+    })
+  })
+
+  it.only("Fluxo de compra - Pagamento com cartão de crédito", () => {
+
+    cy.env(['username', 'password']).then(({ username, password}) => {
+        
+        cy.login(username, password)
+
+        cy.get("button[type='button']")
+            .eq(1)
+            .click()
+
+        cy.get("div[role='dialog']")
+            .should("be.visible")
+
+        cy.get(locators.HOME_BTN)
+            .should("be.visible")
+
+        cy.get("li[class='mb-4'] > div ~ div > div > div > div ~ span")
+            .should("be.visible")
+            .eq(2)
+            .click({ force:true })
+
+        cy.get("a[href='/lybera-shop']")
+            .last()
+            .click()
+
+        cy.get("div[class='py-4']")
+            .find('img')
+            .first()
+            .scrollIntoView()
+
+        cy.contains("p", "Comprar agora")
+            .first()
+            .click()
+
+        cy.get("div[role='none'] ~ div > div > div")
+            .first()
+            .should("be.visible")
+
+        cy.get("img[alt='trophy mapa']")
+            .first()
+            .should("be.visible")
+
+        cy.contains("p", "Fazer pedido")
+            .click()
+
+        cy.get("a[href='/lybera-shop/checkout/payment-method']")
+            .last()
+            .should("be.visible")
+
+        cy.get("a[href='/lybera-shop/checkout/payment-method']")
+            .last()
+            .click()
+
+        cy.get("a[href='/lybera-shop/checkout/payment-method/3']")
+            .last()
+            .should("be.visible")
+
+        cy.get("a[href='/lybera-shop/checkout/payment-method/3']")
+            .last()
+            .click()
+
+        cy.fillCreditCardData()
+
+        /* cy.get("a[href='/lybera-shop/checkout/finalize-order']")
+            .last()
+            .should("be.visible")
+
+        cy.get("a[href='/lybera-shop/checkout/finalize-order']")
+            .last()
+            .click()
+
+        cy.get("div[class='flex'] > div > p ~ div ~ div ~ button")
+            .last()
+            .scrollIntoView()
+
+        cy.get("div[class='flex'] > div > p ~ div ~ div ~ button")
+            .last()
+            .click()
+
+        cy.get("img[alt='Check']")
+            .siblings()
+            .first()
+            .should("have.text", "Pagamento Realizado com sucesso") */
     })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,3 +35,13 @@ Cypress.Commands.add("login", (email, password) => {
     cy.get("a[href='/home']")
   })
 })
+
+Cypress.Commands.add("fillCreditCardData", () => {
+    cy.get("input[name='cardName']").type("Nome Impresso")
+    cy.get("div[class='relative'] > img ~ input").type("4242424242424242")
+    cy.get("input[placeholder='00']").type('01')
+    cy.get("input[placeholder='0000']").type('2035')
+    cy.get("input[placeholder='000']").type('123')
+    //cy.get("button[role='combobox']").click()
+    cy.get("select").select('1', { force: true })
+})
